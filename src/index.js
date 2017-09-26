56b054817b7eed730d7ae49aef1a5283f015d837
ambruzs <peter.ambruzs.ext@nokia.com>
2017-09-26 09:29:44 +0200

    first action and reducer

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@
 import 'babel-polyfill'; // Simplicity in transpiling ES6 gaps. Alternatively import separately (i.e. object.assign)
 import React from 'react';
 import { render } from 'react-dom'; // react-dom separated from Reac in React.14
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
 
 // Dependencies for Router Props
 import { Router, browserHistory } from 'react-router'; // Router Component handles routing. Use HTML5 Push State to handle browserHistory with clean URLs.
@@ -15,7 +17,11 @@ require('./favicon.ico'); // Webpack loads favicon.ico
 import './styles/styles.css'; // Webpack imports Sass/CSS files. Webpack runs associated loader and embeds in page.
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore();
+
 render(
-  <Router history={browserHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
   document.getElementById('app')
 );
